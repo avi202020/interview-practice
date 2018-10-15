c90ef34081988b30e94206c86b0bfb556d9565ef
mark sauer-utley <mark.utley.713@gmail.com>
2018-10-15 12:11:10 -0400

    reverse a string using .reduce

---
+++ reverse.js
@@ -0,0 +1,20 @@
+// Reverse a string without using Array.prototype.reverse()
+
+function reverse(str) {
+  return str.split("").reduce((acc, current) => [current, ...acc], []).join("")
+}
+
+// reverse a string but keep the periods in the same position in the string
+
+function reverseExc(str) {
+  let chars = str.split("")
+  let indexes = []
+
+  chars.forEach((char, idx) => { if (char === ".") { indexes.push(idx) } })
+
+  let filtered = chars.filter(char => char !== ".").reduce((a, b) => [b, ...a], [])
+
+  indexes.forEach(index => filtered.splice(index, 0, "."))
+
+  return filtered.join("")
+}
